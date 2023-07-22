8212d181e606cc7be917f84037876521f723d1d0
silentdiamond <132628168+sefenydiaz@users.noreply.github.com>
2023-07-21 20:09:57 -0400

    folder and file setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="projects-portfolio">
+      {/* <ProjectsPortfolio /> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,4 @@
+// Nav titles need links to sections - About Me, Portfolio, Contact, Resume
+// links need to highlight when hovered over
+// section titles need to be highlighted when jumped to
+//About Me is default section for first page load
