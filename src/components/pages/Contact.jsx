3b50efb83962b9315c60d41c9907d828a95c06ac
silentdiamond <132628168+sefenydiaz@users.noreply.github.com>
2023-07-23 13:31:42 -0400

    added contact form

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -1,12 +1,54 @@
-export default function Contact() {
-    return (
-      <div>
-        <h1>Contact Page</h1>
-        <p>
-      test
-        </p>
-        {/* find bootstrap contact form */}
-      </div>
-    );
-  }
+// function Contact() {
+//   return (
+//     <div>
+//       <h1>Contact</h1>
+//     </div>
+//   )
+// }
+import React from 'react';
+
   
+const Form = () => {
+  const [formStatus, setFormStatus] = React.useState('Send')
+  const onSubmit = (event) => {
+    event.preventDefault()
+    setFormStatus('Submitting...')
+    const { name, email, message } = event.target.elements
+    let contactForm = {
+      name: name.value,
+      email: email.value,
+      message: message.value,
+    } 
+    console.log(contactForm)
+  }
+  return (
+    <div className="container mt-5">
+    <h2 className="mb-3">Contact Me</h2>
+    <form onSubmit={onSubmit}>
+      <div className="mb-3">
+        <label className="form-label" htmlFor="name">
+          Name
+        </label>
+        <input className="form-control" type="text" id="name" required />
+      </div>
+      <div className="mb-3">
+        <label className="form-label" htmlFor="email">
+          Email
+        </label>
+        <input className="form-control" type="email" id="email" required />
+      </div>
+      <div className="mb-3">
+        <label className="form-label" htmlFor="message">
+          Message
+        </label>
+        <textarea className="form-control" id="message" required />
+      </div>
+      <button className="btn btn-danger" type="submit">
+        {formStatus}
+      </button>
+    </form>
+  </div>
+)
+}
+export default Form
+ 
