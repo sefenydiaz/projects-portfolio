7800e70eebf59ecd194930a7c76b042e93413b16
silentdiamond <132628168+sefenydiaz@users.noreply.github.com>
2023-07-24 15:01:09 -0400

    added header

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,11 @@ import './App.css';
 import ProjectsPortfolio from './components/ProjectsPortfolio';
 // import Contact from './components/pages/Contact'
 import Footer from './components/Footer'
-
+import Header from './components/Header'
 function App() {
   return (
     <div className="projects-portfolio">
+      <Header />
       <ProjectsPortfolio />
       {/* <Contact /> */}
       <Footer />
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+const Header = () => {
+    return (
+      <header className="mb-4 py-3 flex-row align-center">
+        <div className="container flex-row justify-space-between-lg justify-center align-center" id="header">
+          <h1 className="m-0">Sefeny Yisell</h1>
+          <p className="m-0">こんにちわ ! Bonjour ! Hola ! 你好 !</p>
+        </div>
+      </header>
+    );
+  };
+  
+  export default Header;
