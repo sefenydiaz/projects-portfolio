6281ed9ee78ee81185324212a48c7ffdfc3fad2a
silentdiamond <132628168+sefenydiaz@users.noreply.github.com>
2023-07-27 15:24:14 -0400

    added bio

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -6,9 +6,9 @@ export default function About() {
         <img src="images/portfolio pic.jpeg" alt="portfolio pic" id="profile" ></img>
         </container>
         <p>
-       test
+       Welcome to my Portfolio! This site is React built and contains several of the recent projects I have worked on. I began coding under Columbia University's coding bootcamp, and have since become familiar with several coding languages. During the bootcamp, I created and collaborated on many succesful group projects and am currently working on a new one now! I have a passion for languages and set a goal to master the development languages as well. I believe myy affinity for translation has contributed to my success in the bootcamp, and I can't wait to learn more! If you would like to collaborate, or just chat with me, please navigate to my Contact page!
         </p>
-        {/* include photo and short bio */}
+        
       </div>
     );
   }
