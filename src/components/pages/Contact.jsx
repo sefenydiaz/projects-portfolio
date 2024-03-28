2e11f8a5d2bf87f45ad185618427b59c274b1c6b
silentdiamond <132628168+sefenydiaz@users.noreply.github.com>
2024-03-28 12:45:51 -0400

    IM FIXED IT

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -1,10 +1,4 @@
-// function Contact() {
-//   return (
-//     <div>
-//       <h1>Contact</h1>
-//     </div>
-//   )
-// }
+
 import React, {useRef} from 'react';
 import emailjs from '@emailjs/browser';
 
@@ -14,51 +8,42 @@ const Contact = () => {
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs.sendForm('service_o348wv5', 'template_endoh32', form.current, 'Noy0bsZ5WdSzWku0ke2ju')
+    emailjs.sendForm('service_o348wv5', 'template_endoh32', form.current, 'xUdregTPH2U4uOMJ6')
       .then((result) => {
           console.log(result.text);
+          e.target.reset();
+          alert('Email Sent!');
       }, (error) => {
           console.log(error.text);
       });
-  };
-  // const [formStatus, setFormStatus] = React.useState('Send')
-  // const onSubmit = (event) => {
-  //   event.preventDefault()
-  //   setFormStatus('Submitting...')
-  //   const { name, email, message } = event.target.elements
-  //   let contactForm = {
-  //     name: name.value,
-  //     email: email.value,
-  //     message: message.value,
-  //   } 
-  //   console.log(contactForm)
-  // }
+  }; 
+
   return (
     <div className="container mt-5">
     <h2 className="mb-3">Contact Me</h2>
-    {/* <form onSubmit={onSubmit}> */}
+  
     <form ref={form} onSubmit={sendEmail}>
       <div className="mb-3">
         <label className="form-label" htmlFor="name">
           Name
         </label>
-        <input className="form-control" type="text" name='from_name' id="name" required />
+        <input className="form-control" type="text" name="from_name" id="name" required />
       </div>
       <div className="mb-3">
         <label className="form-label" htmlFor="email">
           Email
         </label>
-        <input className="form-control" type="email" name='from_email' id="email" required />
+        <input className="form-control" type="email" name="from_email" id="email" required />
       </div>
       <div className="mb-3">
         <label className="form-label" htmlFor="message">
           Message
         </label>
-        <textarea className="form-control" name='message' id="message" required> </textarea>
+        <textarea className="form-control" rows="5" name="message" id="message" required> </textarea>
       </div >
     
-      <button className="btn btn-danger"  type="submit">
-     
+      <button className="submitBtn"  type="submit" value='Send'>
+     Submit
       </button>
       
     </form>
